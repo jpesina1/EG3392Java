bf3ae814d8399177efd3d48375bef0b6253dea07
aktunc-STMU <aktunc@gmail.com>
2017-08-30 00:12:16 -0500

    Looks good

--- TestAutomobile.java
+++ TestAutomobile.java
@@ -6,7 +6,6 @@ public class TestAutomobile {
 		
 		
 		
-		
 		Vehicle truck = new Truck(2009,2500.45,"123abc","Dodge", 1200);
 		Vehicle car = new Car( 2006,2100.56,"1rt245", "Honda",5);
 		Vehicle tank = new Tank(2011,7000.56,4.56);
