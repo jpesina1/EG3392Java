5d0f9d2ce811bab1749a60d333b9320d7a31de6a
jpesina1 <jpesina1@mail.stmarytx.edu>
2017-11-11 22:29:30 -0600

    Add files via upload

--- Assignment 6/CustomFontWindow.java
+++ Assignment 6/CustomFontWindow.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -16,36 +17,35 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-
 public class CustomFontWindow extends JFrame {
 	JPanel panel1, panel2, panel3;
 	JTextField text;
-	JRadioButton arial, times, verdana;  
-	ButtonGroup group;	
+	JRadioButton arial, times, verdana;
+	ButtonGroup group;
 	JCheckBox boldBox, redBox;
 	Font font;
-	
+
 	public CustomFontWindow() {
-		
-		setPreferredSize(new Dimension(400,400));
-		
+
+		setPreferredSize(new Dimension(400, 400));
+
 		panel1 = new JPanel();
-		panel1.setPreferredSize(new Dimension (100,100));
+		panel1.setPreferredSize(new Dimension(100, 100));
 		add(panel1);
 		panel2 = new JPanel();
-		panel2.setPreferredSize(new Dimension(10,25));
-		panel2.setLayout(new GridLayout(1,3));
+		panel2.setPreferredSize(new Dimension(10, 25));
+		panel2.setLayout(new GridLayout(1, 3));
 		add(panel2);
 		panel3 = new JPanel();
-		panel3.setPreferredSize(new Dimension(100,25));
-		//add(panel3);
-		
+		panel3.setPreferredSize(new Dimension(100, 25));
+		// add(panel3);
+
 		font = new Font("Arial", Font.PLAIN, 30);
 		text = new JTextField("Hello");
-		text.setPreferredSize(new Dimension(100,30));
+		text.setPreferredSize(new Dimension(100, 30));
 		text.setFont(font);
 		panel1.add(text);
-		
+
 		arial = new JRadioButton("Arial", true);
 		times = new JRadioButton("Times");
 		verdana = new JRadioButton("Verdana");
@@ -53,67 +53,81 @@ public class CustomFontWindow extends JFrame {
 		group.add(arial);
 		group.add(times);
 		group.add(verdana);
-		
+
 		boldBox = new JCheckBox("Bold");
 		redBox = new JCheckBox("Red");
-		boldBox.addItemListener(new CheckBoxListener());
-		
-		JPanel pane = new JPanel(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
 
-		//For each component to be added to this container:
-		//...Create the component...
-		//...Set instance variables in the GridBagConstraints instance...
-		//pane.add(theComponent, c);
-		
-		
-		
-		
+		boldBox.addItemListener(new CheckBoxListener());
+		redBox.addItemListener(new CheckBoxListener());
+
+
+
 		arial.addActionListener(new RadioButtonListener());
 		times.addActionListener(new RadioButtonListener());
 		verdana.addActionListener(new RadioButtonListener());
-		
+
 		panel2.add(arial);
 		panel2.add(times);
 		panel2.add(verdana);
+		panel3.add(boldBox);
+		panel3.add(redBox);
 		pack();
 		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	private abstract class CheckBoxListener implements ItemListener{
-		public void itemStateChanged(ItemEvent e){
-			if(e.getSource() == boldBox){
-				if(boldBox.isSelected()){
-				text.setFont(arg0);       //bold		`	
+
+	private class RadioButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == arial) {
+				font = new Font("Arial", Font.PLAIN, 30);
+				text.setFont(font);
+			} else if (e.getSource() == times) {
+				font = new Font("Times", Font.PLAIN, 30);
+				text.setFont(font);
+			} else if (e.getSource() == verdana) {
+				font = new Font("Verdana", Font.PLAIN, 30);
+				text.setFont(font);
+			}
+		}
+	}
+		private abstract class CheckBoxListener implements ItemListener {
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getSource() == boldBox) {
+					if (boldBox.isSelected()) {
+						if (e.getSource() == arial) {
+							font = new Font("Arial", Font.BOLD, 30);
+							text.setFont(font);
+						} else if (e.getSource() == times) {
+							font = new Font("Times", Font.BOLD, 30);
+							text.setFont(font);
+						} else if (e.getSource() == verdana) {
+							font = new Font("Verdana", Font.BOLD, 30);
+							text.setFont(font);
+						}
+					} else {
+						if (e.getSource() == arial) {
+							font = new Font("Arial", Font.PLAIN, 30);
+							text.setFont(font);
+						} else if (e.getSource() == times) {
+							font = new Font("Times", Font.PLAIN, 30);
+							text.setFont(font);
+						} else if (e.getSource() == verdana) {
+							font = new Font("Verdana", Font.PLAIN, 30);
+							text.setFont(font);
+						}
+					}
+				} else if (e.getSource() == redBox) {
+					if (redBox.isSelected()) {
+						redBox.setForeground(Color.red);
+					} else {
+						redBox.setForeground(Color.black);
+					}
+
 				}
 			}
 		}
-		
-	}
-	
-	
-	private class RadioButtonListener implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			if (e.getSource() == arial)
-	         {
-	            font = new Font("Arial", Font.PLAIN, 30);
-	            text.setFont(font);
-	         }
-	         else if (e.getSource() == times){
-	        	 font = new Font("Times", Font.PLAIN,30);
-	        	 text.setFont(font);
-	         }
-	         else if(e.getSource() == verdana){
-	        	 font = new Font("Verdana", Font.PLAIN,30);
-	        	 text.setFont(font);
-	         }
-		}
-		
-		
-		
-		
-	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new CustomFontWindow();
